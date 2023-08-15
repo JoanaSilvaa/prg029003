1f2a3db2e290c43cca2af2a8bb75842fb20421bd
Joana <sjoana014@gmail.com>
2023-08-15 15:20:53 -0300

    INCLUSÃO DO FOR

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 //
 // Created by aluno on 11/08/23.
 //
+
 #include <stdio.h>
+#include <string.h>
+int main(int argc, char** argv)
+{
+    char *cpf = (argv [1]);
 
-int main(int argc,char *argv[]) {
-        FILE *arquivo;
-        char *nome[20];
+    char str3[11];
 
-        nome[20] = (argv [2]);
+    for (int i = 0; i < 2; i++) {
+        strncpy ( str3, cpf, 3 );
+        str3[3] = '\0';
 
-        if ((arquivo=fopen (argv[1],"rt"))==NULL)
-            return 0;
-        else
-        {
-            printf("%s",nome[20]);}
-
-        fclose(arquivo);
+        printf("%s.",str3);
+        cpf = cpf+3;
     }
+
+    printf("%s-",str3);
+
+    for (int j = 9; j < 11; ++j) {
+        printf("%c", str3[j]);
+    }
+    return 0;
+}
+
